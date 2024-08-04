0bd6f200232791a2b26b8d657786f8a7bcaf4037
yash537 <yashvijaypatel14@gmail.com>
2024-08-04 16:40:57 +0530

    [deploy]: vercel

--- Frontend/src/axiosConfig.js
+++ Frontend/src/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000", // Update this to match your backend URL
+  // baseURL: "http://localhost:3000", //use this for local host
+  baseURL: "https://todo-backend-beryl-psi.vercel.app",
 });
 
 export default instance;
